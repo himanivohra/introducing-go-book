ed70952de3970f1373bce508d12c7b8447b095b3
himanivohra <34959244+himanivohra@users.noreply.github.com>
2024-06-14 15:26:08 +0530

    Update main.go

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+)
 
 //this is a comment
 
 func main() {
-  fmt.Println("Hello, World")
+  fmt.Println("Hello, my name is Himani")
+  fmt.Println("This is a message before exiting.")
+
+  // Exit the program with a status code
+
+  os.Exit(1)
+
+  /* This line will not be executed because os.Exit terminates the program */
+    fmt.Println("This message will not be printed.")
 }
