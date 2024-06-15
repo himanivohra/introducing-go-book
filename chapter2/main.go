a0c39125820a04dba09a562cbc468578a278829e
himanivohra <34959244+himanivohra@users.noreply.github.com>
2024-06-15 20:01:03 +0530

    Update main.go

--- chapter2/main.go
+++ chapter2/main.go
@@ -1,1 +1,26 @@
+package main
 
+import "fmt"
+
+// Intergers
+
+func main() {
+	fmt.Println("1 + 1 =", 1+1)
+
+	// Floating-point numbers
+	fmt.Println("1 + 1 =", 1.0+1.0)
+
+	// Strings
+	fmt.Println(len("Hello, World"))
+	fmt.Println("Hello, World"[1])
+	fmt.Println("Hello," + " World")
+
+	// Booleans
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+
+}
